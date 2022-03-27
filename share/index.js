49ae5c993f93a3920ed16c2a49df5f150c9e1c6a
Isaac Kogan <isaacikogan@gmail.com>
2022-03-26 21:06:10 -0400

    Update page

---
+++ share/index.js
@@ -0,0 +1,14 @@
+const share = (
+    new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    })
+)["share"];
+
+switch (share) {
+
+    case ("twitter"): {
+        
+    }
+
+}
+
